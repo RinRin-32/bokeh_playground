7264295d6581d33496145482cc85f642974e7d98
rinintachuen <burin.ina@student.mahidol.edu>
2025-01-09 14:44:10 +0900

    way much better line

--- visualizer/decisionboundary.py
+++ visualizer/decisionboundary.py
@@ -3,6 +3,8 @@ from bokeh.layouts import column, row
 from bokeh.models import Div, Button, ColumnDataSource, GlyphRenderer, Image
 import numpy as np
 
+from skimage import measure  # To extract contour lines
+
 class DecisionBoundaryVisualizer:
     def __init__(self, model, shared_source):
         self.model = model
@@ -14,22 +16,37 @@ class DecisionBoundaryVisualizer:
         self.classes = np.unique(self.y) 
         self.message_div = Div(text="", width=400, height=50, styles={"color": "red"})
 
-        self.plot = figure(title="Interactive 2D Classification Visualization", width=600, height=600, tools="tap,box_select")
+        # Calculate fixed x_range and y_range based on initial data
+        x_min, x_max = self.X[:, 0].min() - 1, self.X[:, 0].max() + 1
+        y_min, y_max = self.X[:, 1].min() - 1, self.X[:, 1].max() + 1
 
-        self.boundary_x, self.boundary_y, zz = self.calculate_boundaries(self.X, self.y)
+        # Fix the graph size using x_range and y_range
+        self.plot = figure(
+            title="Interactive 2D Classification Visualization", 
+            width=600, height=600, 
+            tools="tap,box_select",
+            x_range=(x_min, x_max), 
+            y_range=(y_min, y_max)
+        )
+
+        self.boundary_source = ColumnDataSource(data=dict(xs=[], ys=[]))
 
         self.plot.scatter("x", "y", size=8, source=self.source, color="color", marker="marker")
-        if self.boundary_x is not None and self.boundary_y is not None:
-            self.plot.contour(x=self.boundary_x, y=self.boundary_y, z = zz, levels=np.linspace(self.X[:, 0].min() - 1, self.X[:, 0].max() + 1, 10), line_width=2, line_color="black")
-        
+        self.plot.multi_line(xs="xs", ys="ys", source=self.boundary_source, line_width=2, color="black")
+
+        # Calculate boundaries and update immediately
+        xx, yy, zz = self.calculate_boundaries(self.X, self.y)
+        self.update_boundary(xx, yy, zz)
+
+        # Set up callback for source updates
         self.source.on_change('data', self.update)
-        
+
     def calculate_boundaries(self, X, y):
         print("Calculating boundaries...")
         unique_classes = np.unique(y)
         if len(unique_classes) < 2:
             self.message_div.text = "Error: At least two classes are required to fit the model."
-            return None, None
+            return None, None, None
         else:
             self.message_div.text = ""
             self.model.fit(X, y)
@@ -37,18 +54,29 @@ class DecisionBoundaryVisualizer:
             x_min, x_max = self.X[:, 0].min() - 1, self.X[:, 0].max() + 1
             y_min, y_max = self.X[:, 1].min() - 1, self.X[:, 1].max() + 1
             xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01), 
-                                np.arange(y_min, y_max, 0.01))
+                                 np.arange(y_min, y_max, 0.01))
             
             grid = np.c_[xx.ravel(), yy.ravel()]
             zz = self.model.predict(grid)
             zz = zz.reshape(xx.shape)
             
-            #boundary_mask = np.isclose(zz, 0.7, atol=0.04)
-            #boundary_x, boundary_y = xx[boundary_mask], yy[boundary_mask]
-
             return xx, yy, zz
 
-        
+    def extract_boundary_lines(self, xx, yy, zz):
+        contours = measure.find_contours(zz, level=0.5)  # Assuming boundary at 0.5 probability
+        xs, ys = [], []
+        for contour in contours:
+            xs.append(xx[0, 0] + contour[:, 1] * (xx[0, -1] - xx[0, 0]) / zz.shape[1])
+            ys.append(yy[0, 0] + contour[:, 0] * (yy[-1, 0] - yy[0, 0]) / zz.shape[0])
+        return xs, ys
+
+    def update_boundary(self, xx, yy, zz):
+        if xx is not None and yy is not None and zz is not None:
+            xs, ys = self.extract_boundary_lines(xx, yy, zz)
+            self.boundary_source.data = {"xs": xs, "ys": ys}
+        else:
+            self.boundary_source.data = {"xs": [], "ys": []}
+
     def update(self, attr, old, new):
         mask = np.array(self.source.data["color"]) != "grey"
         x_new = np.array(self.source.data["x"])[mask]
@@ -57,19 +85,8 @@ class DecisionBoundaryVisualizer:
         X_new = np.vstack((x_new, y_new)).T
         y_new = np.array(self.source.data["class"])[mask].flatten()
 
-        self.boundary_x, self.boundary_y, zz = self.calculate_boundaries(X_new, y_new)
-        if self.boundary_x is not None and self.boundary_y is not None:
-    
-            for renderer in self.plot.renderers:
-                if isinstance(renderer, (GlyphRenderer, Image)):
-                    renderer.glyph.line_alpha = 0.3  # Reduce line visibility
-                    # For some reason, on first launch, the line wouldn't reduce visibility but launching the server after that solves it?
+        xx, yy, zz = self.calculate_boundaries(X_new, y_new)
+        self.update_boundary(xx, yy, zz)
 
-            self.plot.renderers = [r for r in self.plot.renderers if not isinstance(r, Image)]
-            if len(self.plot.renderers) > 2:
-                self.plot.renderers.remove(self.plot.renderers[-1])
-            
-            self.plot.contour(x=self.boundary_x, y=self.boundary_y, z=zz, levels=np.linspace(self.X[:, 0].min() - 1, self.X[:, 0].max() + 1, 10), line_width=2, line_color="black")
-        
     def get_layout(self):
         return column(self.plot, self.message_div)
