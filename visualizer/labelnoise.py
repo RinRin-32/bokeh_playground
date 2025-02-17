9bebd1e06bcda6a8ff90205d8ac9b68a04fdd8b9
RinRin-32 <rinrin.int@proton.me>
2025-02-17 14:22:45 +0900

    color select for label noise plot

--- visualizer/labelnoise.py
+++ visualizer/labelnoise.py
@@ -1,10 +1,11 @@
 from bokeh.plotting import figure
 from bokeh.layouts import column, row
-from bokeh.models import HoverTool, ColumnDataSource, Div, CustomJS, Select
+from bokeh.models import HoverTool, ColumnDataSource, Div, CustomJS, Select, Button
 
 class LabelNoisePlot:
     def __init__(self, shared_source):
         self.shared_source = shared_source
+        
         self.unique_labels = list(set(shared_source.data['label']))
         self.unique_labels.sort()
         self.unique_labels.insert(0, "All")  # Add an "All" option
@@ -12,13 +13,13 @@ class LabelNoisePlot:
         self.filtered_source = ColumnDataSource(data=self.shared_source.data.copy())
         self.plot = self.create_plot()
         self.selected_source = ColumnDataSource(data=dict(img=[], label=[]))
-
+        
         self.image_display = Div(text=self.generate_html([], []), width=300, height=600)
-
+        
         self.dropdown = Select(title="Select Class:", value="All", options=self.unique_labels)
         self.dropdown.js_on_change("value", CustomJS(args=dict(source=self.shared_source, filtered_source=self.filtered_source, dropdown=self.dropdown), code="""
             var selected_class = dropdown.value;
-            var new_data = {x: [], y: [], label: [], img: []};
+            var new_data = {x: [], y: [], label: [], img: [], color: []};
             
             for (var i = 0; i < source.data['label'].length; i++) {
                 if (selected_class === 'All' || source.data['label'][i] === selected_class) {
@@ -26,28 +27,39 @@ class LabelNoisePlot:
                     new_data['y'].push(source.data['y'][i]);
                     new_data['label'].push(source.data['label'][i]);
                     new_data['img'].push(source.data['img'][i]);
+                    new_data['color'].push(source.data['color'][i]);
                 }
             }
             
             filtered_source.data = new_data;
         """))
-
+        
         self.callback = CustomJS(args=dict(source=self.filtered_source, selected_source=self.selected_source, display=self.image_display), code="""
             var selected_indices = source.selected.indices;
             var imgs = [];
             var labels = [];
+            
+            for (var i = 0; i < source.data['x'].length; i++) {
+                source.data['color'][i] = 'red';  // Reset all to red
+            }
+            
             for (var i = 0; i < selected_indices.length; i++) {
+                source.data['color'][selected_indices[i]] = 'grey';  // Selected points become grey
                 imgs.push(source.data['img'][selected_indices[i]]);
                 labels.push(source.data['label'][selected_indices[i]]);
             }
+            
+            source.change.emit();
+            
             var html = "<div style='display: flex; flex-wrap: wrap; gap: 10px;'>";
             for (var i = 0; i < imgs.length; i++) {
                 html += `<div><img src='data:image/png;base64,${imgs[i]}' width='56' height='56'><br>Label: ${labels[i]}</div>`;
             }
             html += "</div>";
             display.text = html;
-        """)
-
+        """
+        )
+        
         self.filtered_source.selected.js_on_change("indices", self.callback)
 
     def create_plot(self):
@@ -59,7 +71,7 @@ class LabelNoisePlot:
         p.yaxis.axis_label_text_font_size = "20pt"
         p.xaxis.axis_label_text_font_size = "20pt"
 
-        p.scatter("x", "y", source=self.filtered_source, size=6, color="red", legend_label="Data", fill_alpha=0.6)
+        p.scatter("x", "y", source=self.filtered_source, size=6, color="color", legend_label="Data", fill_alpha=0.6)
 
         hover = HoverTool(tooltips="""
             <div>
