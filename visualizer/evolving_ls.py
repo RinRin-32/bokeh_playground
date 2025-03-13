d3213e2eca4ca5bdb8458e006d1e1bd3caba3b05
RinRin-32 <rinrin.int@proton.me>
2025-03-13 12:21:00 +0900

    resolve plot scaling and newline issue

--- visualizer/evolving_ls.py
+++ visualizer/evolving_ls.py
@@ -1,13 +1,10 @@
+from bokeh.models import ColumnDataSource, CustomJS, Div
 from bokeh.plotting import figure
-from bokeh.layouts import column, row
-from bokeh.models import HoverTool, ColumnDataSource, Div, CustomJS, Select
-from visualizer.evolvingmpe import EvolvingMemoryMapVisualizer
-from collections import defaultdict
-
+from bokeh.layouts import row
 
 class EvolvingLabelNoisePlot:
-    def __init__(self, shared_soruce, plot_name, y_range, n_sample):
-        self.shared_source = shared_soruce
+    def __init__(self, shared_source, plot_name, y_range, n_sample):
+        self.shared_source = shared_source
         self.plot_name = plot_name
         self.n_sample = n_sample
 
@@ -19,7 +16,7 @@ class EvolvingLabelNoisePlot:
         self.image_display = Div(
             text="<h3>Selected Images:</h3>", 
             sizing_mode="stretch_width", height=500, 
-            stylesheets=[""" .scroll-box { overflow-y: auto; max-height: 600px; padding: 10px; } """], 
+            stylesheets=[""" .scroll-box { overflow-y: auto; max-height: 500px; padding: 10px; } """], 
             css_classes=["scroll-box"]
         )
 
@@ -37,59 +34,59 @@ class EvolvingLabelNoisePlot:
         
         p.title.text_font_size = "25px"
         p.title.align = 'center'
-        p.xaxis.visible = False
         p.yaxis.major_label_text_font_size = "15pt"
         p.yaxis.axis_label_text_font_size = "20pt"
-        p.xaxis.axis_label_text_font_size = "20pt"
-        p.xaxis.visible=True
-        p.xaxis.major_label_text_font_size = "0pt"
-        p.min_border_bottom = 50
 
         p.scatter("x", "y", source=self.shared_source, size=6, color="color", legend_label="Data", fill_alpha=0.6)
-
-        hover = HoverTool(tooltips="""
-                <div>
-                    <img src="data:image/png;base64,@img" width="28" height="28"></img>
-                    <br>
-                    <b>Label:</b> @label
-                    <br>
-                    <img src="data:image/png;base64,@noise_chart" width="150" height="100"></img>
-                </div>
-        """)
-        p.add_tools(hover)
 
         return p
     
     def setup_callbacks(self):
-        self.shared_source.selected.js_on_change("indices", CustomJS(args={"source": self.shared_source, "image_display": self.image_display}, code="""
+        callback_code = """
             var indices = source.selected.indices;
             var images = source.data["img"];
             var labels = source.data["label"];
-            
-            var groupedImages = {};
-            
-            // Collect images by label
-            for (var i = 0; i < indices.length; i++) {
-                var label = labels[indices[i]];
-                var imgTag = "<img src='data:image/png;base64," + images[indices[i]] + "' width='64' height='64'>";
+            var noise_charts = source.data["noise_chart"];
+            var html = "";
+
+            if (indices.length === 0) {
+                html += "<h3>No selection made.</h3>";
+            } else {
+                // Selection exists: Show selected images
+                var groupedImages = {};
+                html += "<h3>Selected Images:</h3>";
                 
-                if (!(label in groupedImages)) {
-                    groupedImages[label] = [];
+                for (var i = 0; i < indices.length; i++) {
+                    var label = labels[indices[i]];
+                    var imgTag = "<img src='data:image/png;base64," + images[indices[i]] + "' width='64' height='64'>";
+                    var noiseChartTag = "<img src='data:image/png;base64," + noise_charts[indices[i]] + "' width='150' height='100'>";
+                    var wrappedTag = "<span style='display: inline-block; margin: 5px;'>" + imgTag + noiseChartTag + "</span>";
+
+                    if (!(label in groupedImages)) {
+                        groupedImages[label] = [];
+                    }
+                    groupedImages[label].push(wrappedTag);
                 }
-                groupedImages[label].push(imgTag);
+
+                var sortedLabels = Object.keys(groupedImages).sort();
+                sortedLabels.forEach(function(label) {
+                    html += "<div style='margin-bottom:10px; max-width: 100%;'><b>Label: " + label + "</b><br>";
+                    html += "<div style='display: flex; flex-wrap: wrap; gap: 5px;'>" + groupedImages[label].join("") + "</div></div>";
+                });
             }
-            
-            // Construct the HTML grouped by label
-            var html = "<h3>Selected Images:</h3>";
-            var sortedLabels = Object.keys(groupedImages).sort();
-            
-            sortedLabels.forEach(function(label) {
-                html += "<div style='margin-bottom:10px;'><b>Label: " + label + "</b><br>";
-                html += groupedImages[label].join(" ") + "</div>";
-            });
 
             image_display.text = '<div class="scroll-box">' + html + '</div>';
-        """))
+        """
+
+        self.shared_source.selected.js_on_change("indices", CustomJS(args={
+            "source": self.shared_source, 
+            "image_display": self.image_display
+        }, code=callback_code))
+
+        self.shared_source.js_on_change("data", CustomJS(args={
+            "source": self.shared_source, 
+            "image_display": self.image_display
+        }, code=callback_code))
     
     def get_layout(self):
-        return row(self.plot, self.image_display,)
+        return row(self.plot, self.image_display)
