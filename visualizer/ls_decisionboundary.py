e44952274493f5b5bbda552cd58e49444adcf376
RinRin-32 <rinrin.int@proton.me>
2025-04-11 11:52:23 +0900

    reduced tools

--- visualizer/ls_decisionboundary.py
+++ visualizer/ls_decisionboundary.py
@@ -38,9 +38,8 @@ class LSBoundaryVisualizer:
             #sizing_mode="scale_both",
             x_range=(x_min, x_max),
             y_range=(y_min, y_max),
-            tools="tap, box_select, reset, pan, wheel_zoom",
-            active_drag="pan",
-            active_scroll="wheel_zoom"
+            tools="tap, box_select",
+            active_drag="box_select",
         )
 
         initial_xs = shared_resource.data["xs"][0]
